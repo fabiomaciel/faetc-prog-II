da775ab3c07a2feed9bce6a64c8a02f53979b394
Fabio Maciel <fabio.souza@metasix.com.br>
2016-04-27 17:27:19 -0300

    Added topic-2 ex 4

---
+++ topic-2/soma_numeros.c
@@ -0,0 +1,14 @@
+/**
+ * Faça um programa que peça dois números e devolva a soma deles.
+ *
+ */
+
+#include <stdio.h>
+
+int main(void) {
+  int n1, n2;
+  scanf("%d", &n1);
+  scanf("%d", &n2);
+  printf("%d", n1+n2);
+  return 0;
+}
