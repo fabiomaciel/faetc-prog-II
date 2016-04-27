90f325f57b972fdff1b7d780c83019a7f0bb9943
Fabio Maciel <fabio.souza@metasix.com.br>
2016-04-27 17:27:02 -0300

    Added topic-2 ex 3

---
+++ topic-2/impressao_sequencia.c
@@ -0,0 +1,33 @@
+/**
+ *
+ * Faça um programa que exiba a sequência de caracteres abaixo
+ *
+ * \n \xDC\xDC\xDB\xDB\xDB\xDB\xDC\xDC
+ *
+ * \n \xDFO\xDF\xDF\xDF\xDFO\xDF
+ *
+ * \n\n
+ *
+ * \n \xDC\xDC\xDB \xDB\xDB\xDB\xDB\xDB\xDB
+ *
+ * \n \xDFO\xDF\xDF\xDF\xDF\xDFOO\xDF
+ *
+ * \n\n
+ *
+ */
+
+#include<stdio.h>
+
+int main(){
+
+  printf("%s\n%s\n%s\n%s\n%s\n%s\n",
+         "\\n \\xDC\\xDC\\xDB\\xDB\\xDB\\xDB\\xDC\\xDC",
+         "\\n \\xDFO\\xDF\\xDF\\xDF\\xDFO\\xDF",
+         "\\n\\n",
+         "\\n \\xDC\\xDC\\xDB \\xDB\\xDB\\xDB\\xDB\\xDB\\xDB",
+         "\\n \\xDFO\\xDF\\xDF\\xDF\\xDF\\xDFOO\\xDF",
+         "\\n\\n"
+         );
+
+  return 1;
+}
