8653e84f7326af12b39dfbf615177e00046593d0
Fabio Maciel <fabio.souza@metasix.com.br>
2016-04-27 17:22:18 -0300

    added topic-2 1 exercicio

---
+++ topic-2/impressao_dados.c
@@ -0,0 +1,15 @@
+/**
+ * Exercicio - Impressão de dados em linhas diferentes
+ * Faça um programa que imprima as palavras UM, DOIS, TRES
+ * no formato abaixo, utilizando apenas uma instrução printf. UM DOIS TRES
+ * UM
+ *    DOIS
+ *       TRES
+ * */
+
+#include <stdio.h>
+
+int main(){
+  printf("UM\n   DOIS\n      TRES\n");
+  return 1;
+}
