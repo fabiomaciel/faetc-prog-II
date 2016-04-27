f64788b1e6744ba99a7bf704d24cd39be845eb6c
Fabio Maciel <fabio.souza@metasix.com.br>
2016-04-27 18:15:37 -0300

    Added topic-4 ex2

---
+++ topic-4/triangulo.c
@@ -0,0 +1,39 @@
+/**
+ * Exercício - Programa para desenho de um triângulo
+ * Faça um programa que imprima um triangulo conforme abaixo:
+ *
+ * *
+ *
+ * **
+ *
+ * ***
+ *
+ * ****
+ *
+ * *****
+ *
+ * Utilize o loop for na sua programação
+ *
+ * Desafio : Modifique o programa anterior para que seja solicitado ao usuário um número,
+ * que será o comprimento e a altura do triangulo.
+ *
+ */
+
+#include<stdio.h>
+
+int main(){
+
+  int altura;
+
+   scanf("%d", &altura);
+
+   int i, j;
+   for(i = 0; i < altura; i++){
+     for(j = 0; j <= i; j++){
+       printf("*");
+     }
+     printf("\n");
+   }
+
+  return 0;
+}
