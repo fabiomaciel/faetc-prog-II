4f58718f9ccb1e0954245ee00b4eb2261feef0dd
Fabio Maciel <fabio.souza@metasix.com.br>
2016-04-28 14:41:21 -0300

    Added topic-5 ex1

---
+++ topic-5/celsius2fahren.c
@@ -0,0 +1,9 @@
+/**
+ * Faça uma função que receba um número float com
+ * a quantidade de graus Celsius e retorne o valor em graus Fahrenheit (float).
+ *
+ */
+
+float celsius2faren(float c){
+  return (9/5.) * c + 32;
+}
