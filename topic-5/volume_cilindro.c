6fa6e3e61725a35b255b7815be487702df60b431
Fabio Maciel <fabio.souza@metasix.com.br>
2016-04-28 17:33:01 -0300

    Added topic-5 ex4

---
+++ topic-5/volume_cilindro.c
@@ -0,0 +1,11 @@
+/**
+ * Faça uma função para calcular o volume de um cilindro,
+ * a qual receberá dois parâmetros float (raio e altura) e retornará o volume (float)
+ *
+ */
+
+#define M_PI 3.14159265358979323846
+
+float volume_cilindro(float r, float h){
+  return M_PI * r * 2 * h;
+}
